2caaabfae4c45f66a35320982ee5ed102afbbe5c
vikash <44chansw@gmail.com>
2022-09-23 15:18:58 +0530

    routing done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { prisma } from "./prisma";
 
 const app = express();
 
@@ -10,5 +11,15 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+app.get("/", async (_req: Request, res: Response) => {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
+  res.json(posts);
+});
 
 export default app;
