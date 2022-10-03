1e41926b2d67943945e2310b75d709f6a2e56c30
vikash <44chansw@gmail.com>
2022-10-03 21:12:52 +0530

    added api url

--- client/src/uttils/makeRequest.ts
+++ client/src/uttils/makeRequest.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://four4chanbackend.onrender.com",
 });
 
 export const makeRequest = async (url: string, options: any) => {
